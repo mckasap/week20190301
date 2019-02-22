84931908ae3076df83dd257a620d52d9ca3c019b
mckasap <mckasap@gmail.com>
2019-02-22 14:42:32 +0300

    week03 foto

--- app/src/main/java/org/kasapbasi/week20190301/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week20190301/MainActivity.java
@@ -2,6 +2,9 @@ package org.kasapbasi.week20190301;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void FotoCek(View v){
+
+        Toast.makeText(this," ŞİPŞAK",Toast.LENGTH_LONG).show();
     }
 }
